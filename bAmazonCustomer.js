8a479ed10f31c5fad37895b9a4fb04308fffaa41
Janet Warren <jdublu416@gmail.com>
2018-04-21 00:03:43 -0400

    update files and database rows

--- bAmazonCustomer.js
+++ bAmazonCustomer.js
@@ -16,13 +16,47 @@ var connection = mysql.createConnection({
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
    
-     displayItems();
-
+    
+    
+    displayItems();
+    
   });
+  
+  
+// itemSearch();
 
-  var 
-
-  function displayItems(){
-
+  function  displayItems() {
+    console.log("Selecting all products...\n");
+    var sql="SELECT ITEM_ID, PRODUCT_NAME, PRICE, Stock_quantity FROM products"
+    connection.query(sql, function(err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.log(res);
+      console.log("this var is"+res[0].item_id);
+      // for (var i=0; i<res.length;i++)
+      // {
+      //   console.log (`Item for Sale: ${res[i].value(item_id)}
+      //   `)
+      // }  
+  
+      connection.end();
+    });
   }
+
+  function itemSearch() {
+    inquirer
+      .prompt({
+        name: "choice",
+        type: "input",
+        message: "What item would you like to purchase? Please select an item number..."
+      })
+      .then(function(choice) {
+        var stock_quantity;
+        var query = "SELECT ITEM_ID, STOCK_QUANTITY FROM PRODUCT WHERE ?";
+        connection.query(query, { choice: item_id }, function(err, res) {
+         return stock_quantity;
+        });
+      });
+  }
+  
   
