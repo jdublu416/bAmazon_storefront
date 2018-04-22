da67f50a67f493118c7fc654e8943bdbe2f517ac
Janet Warren <jdublu416@gmail.com>
2018-04-22 19:52:14 -0400

    testing of code completed and update to read me

--- bAmazonCustomer.js
+++ bAmazonCustomer.js
@@ -2,6 +2,7 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var STOCK_QUANTITY;
 var ITEM_ID;
+var chosenItem;
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -22,15 +23,13 @@ connection.connect(function(err) {
   displayItems();
 });
 
-// itemSearch();
-
 function displayItems() {
   console.log("Selection of all products available for purchase...\n");
   var sql = "SELECT ITEM_ID, PRODUCT_NAME, PRICE, STOCK_QUANTITY FROM products";
   connection.query(sql, function(err, res) {
     if (err) throw err;
     // Log all results of the SELECT statement
-    // console.log(res);
+
     console.log("this item_id is " + res[0].ITEM_ID);
 
     for (var i = 0; i < res.length; i++) {
@@ -41,7 +40,6 @@ function displayItems() {
         `);
     }
     itemSearch();
-    connection.end();
   });
 }
 
@@ -54,23 +52,20 @@ function itemSearch() {
         "What item would you like to purchase? Please select an Item ID#..."
     })
     .then(function(answer) {
+      connection.query(
+        "SELECT * FROM PRODUCTS WHERE ?",
+        { ITEM_ID: answer.choice },
+        function(err, res) {
+          chosenItem = res[0];
+          var stock_quantity = res[0].STOCK_QUANTITY;
 
-      
-      console.log(answer.choice);
-      connection.query("SELECT * FROM PRODUCT WHERE ?", {ITEM_ID: answer.choice}, function(err, res) {
-        console.log("The results are "+res);
-      
-        var chosenItem = res[0].ITEM_ID;
-        var stock_quantity=res[0].STOCK_QUANTITY;
-          
-        
-      });
-      purchaseItem(stock_quantity);
-      connection.end();
+          purchaseItem(chosenItem);
+        }
+      );
     });
 }
 
-function purchaseItem(stock_quantity) {
+function purchaseItem(chosenItem) {
   inquirer
     .prompt({
       name: "userPurchase",
@@ -78,15 +73,36 @@ function purchaseItem(stock_quantity) {
       message: "How many would you like to purchase?"
     })
     .then(function(answer) {
-      console.log(stock_quantity + " " + answer.userPurchase);
-      if (stock_quantity < answer.userPurchase) {
+      // console.log(stock_quantity + " " + answer.userPurchase);
+      if (chosenItem.STOCK_QUANTITY < answer.userPurchase) {
         console.log(
           "Insufficient inventory...We only have " +
-            stock_quantity +
+            chosenItem.STOCK_QUANTITY +
             " items for sale!"
         );
+        purchaseItem(chosenItem);
       } else {
-        console.log("do something else");
+        var changeSQ = chosenItem.STOCK_QUANTITY - answer.userPurchase;
+        var salePrice= chosenItem.PRICE * answer.userPurchase;
+        console.log("Your order is just about complete  \nTotal cost will be "+ salePrice);
+        updateDB(changeSQ);
       }
     });
+}
+
+function updateDB(changeSQ) {
+  connection.query(
+    "UPDATE PRODUCTS SET ? WHERE ?",
+    [
+      {
+        STOCK_QUANTITY: changeSQ
+      },
+      { ITEM_ID: chosenItem.ITEM_ID }
+    ],
+    function(err) {
+      if (err) throw err;
+      console.log("Congratulations! Order submitted...You successfully purchased your item!");
+      connection.end();
+    }
+  );
 }
